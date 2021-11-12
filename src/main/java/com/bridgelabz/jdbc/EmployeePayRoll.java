ff126030842604cb946f776eb92e241299bb7dba
Ashish <89019572+aajisahu@users.noreply.github.com>
2021-11-12 16:34:36 +0530

    Update EmployeePayRoll.java

--- src/main/java/com/bridgelabz/jdbc/EmployeePayRoll.java
+++ src/main/java/com/bridgelabz/jdbc/EmployeePayRoll.java
@@ -16,34 +16,46 @@ public class EmployeePayRoll {
 		final int EXIT = 10;
 		int choice = 0;
 		while (choice != EXIT) {
-			System.out.println("enter your choice\n1. Execute query\n2. update basic pay\n3. display employee roll" +
-					"\n4. select range of employee\n5. calculate\n0. EXIT\n");
+			System.out.println("Enter Your Choice\n1. Get employee data\n2. update basic pay\n3. display employee roll\n4. empdata range  \n5. calculate \n6. EXIT\n");
 			choice = scanner.nextInt();
 			switch (choice) {
-			case 0 : System.out.println("good bye");
-			case 1 : {
+			case 1:
 				String query = "select * from employee_payroll";
 				employeePayRollService.queryExecute(query);
 				employeePayRollService.display();
-			}
-			case 2 : {
+				break;
+
+			case 2:
 				System.out.println("enter employee name");
 				String empName = scanner.next();
 				System.out.println("enter basic pay you want to update");
-				double basicPay = scanner.nextDouble();
-				employeePayRollService.updateBasicPay(empName, basicPay);
-			}
-			case 3 : employeePayRollService.display();
-			case 4 : {
+				double basic_pay = scanner.nextDouble();
+				employeePayRollService.updateBasicPay(empName, basic_pay);
+				break;
+
+			case 3:
+				employeePayRollService.display();
+				break;
+
+			case 4:
 				System.out.println("enter initial date");
 				LocalDate iDate = LocalDate.parse(scanner.next());
 				System.out.println("enter final date");
 				LocalDate eDate = LocalDate.parse(scanner.next());
-				employeePayRollService.selectEmployee(iDate,eDate);
-			}
-			case 5: employeePayRollService.calculate();
+				employeePayRollService.selectEmployee(iDate, eDate);
+				break;
+
+			case 5:
+				employeePayRollService.calculate();
+				break;
+
+			case 6:
+				System.out.println("Good Bye");
+				System.exit(0);
 
 			}
 		}
 	}
+	
+
 }
