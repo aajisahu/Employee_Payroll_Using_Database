3c92a5ddfeb2c180ff6507823394850234a65e65
Ashish <89019572+aajisahu@users.noreply.github.com>
2021-11-13 15:32:53 +0530

    Added: Find sum,avg,min,max and count from employee payroll

--- src/main/java/com/bridgelabz/jdbc/EmployeeConfig.java
+++ src/main/java/com/bridgelabz/jdbc/EmployeeConfig.java
@@ -9,27 +9,30 @@ import java.sql.SQLException;
  * @author ASHISH SAHU
  *
  */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class EmployeeConfig {
 
-    private static Connection connection = null;
-/**
- * Using static block creating the connection
- */
-    static {
-        String URL_JD = "jdbc:mysql://localhost:3306/payroll_service";
-        String USER_NAME = "root";
-        String PASSWORD = "root";
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(URL_JD, USER_NAME, PASSWORD);
-        } catch (ClassNotFoundException e) {
-            throw new EmployeeException("invalid driver");
-        } catch (SQLException throwable) {
-            throw new EmployeeException("Invalid get connection parameters");
-        }
-    }
-    public static Connection getConfig(){
-        return connection;
-    }
+	private static Connection connection = null;
+
+	static {
+		String URL_JD = "jdbc:mysql://localhost:3306/payroll_service";
+		String USER_NAME = "root";
+		String PASSWORD = "root";
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection(URL_JD, USER_NAME, PASSWORD);
+		} catch (ClassNotFoundException e) {
+			throw new EmployeeException("invalid driver");
+		} catch (SQLException throwable) {
+			throw new EmployeeException("Invalid get connection parameters");
+		}
+	}
+
+	  public static Connection getConfig(){
+	        return connection;
+	    }
+
 }
